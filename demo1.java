a7900711e2b7773cecce30d279954a12be81db22
Nikhil_Dev <nikhilkoundinya5@gmail.com>
2025-04-23 06:03:55 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,17 @@
+class demo1{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+
+        if (a>b && a>c) {
+            System.out.println("a is greater");
+        }else if (b>c) {
+            System.out.println("b is greater");
+        }else{
+            System.out.println("c is greater");
+        }
+
+
+    }
+}
